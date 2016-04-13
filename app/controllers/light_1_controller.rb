5d527df3ec9097a7342ee652fcc4fbfa7f668526
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-13 08:57:41 +0200

    feat(light_1): use WiringPi to write on serial port

--- app/controllers/light_1_controller.rb
+++ app/controllers/light_1_controller.rb
@@ -1,7 +1,26 @@
+#
+# Send order to Arduino through serial port
+#
 class Light1Controller < ApplicationController
+  before_action :open_serial_communication
+  after_action :close_serial_communication
+
   def on
+    @serial.serial_puts(1)
   end
 
   def off
+    @serial.serial_puts(0)
   end
+
+  private
+
+  def open_serial_communication
+    @serial = WiringPi::Serial.new(Arduino::DEVICE, Arduino::SERIAL_BAUD_RATE)
+  end
+
+  def close_serial_communication
+    @serial.serial_close
+  end
+
 end
