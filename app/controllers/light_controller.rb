80db23a1dab7b3587f25a4ee68b55995f9bf6658
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-16 22:45:54 +0200

    feat(controller): init serial communication on application start instead of on every request

--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -2,27 +2,15 @@
 # Send order to Arduino through serial port
 #
 class LightController < ApplicationController
-  before_action :open_serial_communication
-  after_action :close_serial_communication
 
   def on
-    @serial.serial_puts("0")
+    Arduino::SERIAL.serial_puts("0")
     render json: { light: "on" }
   end
 
   def off
-    @serial.serial_puts("1")
+    Arduino::SERIAL.serial_puts("1")
     render json: { light: "off" }
-  end
-
-  private
-
-  def open_serial_communication
-    @serial = WiringPi::Serial.new(Arduino::DEVICE, Arduino::SERIAL_BAUD_RATE)
-  end
-
-  def close_serial_communication
-    @serial.serial_close
   end
 
 end
--- config/initializers/arduino.rb
+++ config/initializers/arduino.rb
@@ -3,4 +3,6 @@ module Arduino
   DEVICE = '/dev/ttyACM0'
   SERIAL_BAUD_RATE = 9600
 
+  SERIAL = WiringPi::Serial.new(Arduino::DEVICE, Arduino::SERIAL_BAUD_RATE)
+
 end
