2a0b653b157d37e9b026837164343aaf61e7c5b4
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-17 15:21:22 +0200

    feat(light): create html views for #on #off actions

--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -5,12 +5,10 @@ class LightController < ApplicationController
 
   def on
     Arduino::SERIAL.serial_puts("0")
-    render json: { light: "on" }
   end
 
   def off
     Arduino::SERIAL.serial_puts("1")
-    render json: { light: "off" }
   end
 
 end
