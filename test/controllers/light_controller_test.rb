759f262866ced0cdc12092077346d3f60ebffe96
Arnaud Lenglet <arlenglet@gmail.com>
2016-04-18 20:45:46 +0200

    test(controller): update test due to switch from JSON to HTML

--- test/controllers/light_controller_test.rb
+++ test/controllers/light_controller_test.rb
@@ -4,13 +4,11 @@ class LightControllerTest < ActionController::TestCase
   test "should get on" do
     get :on
     assert_response(:success)
-    assert_equal(JSON.parse(response.body)['light'], 'on')
   end
 
   test "should get off" do
     get :off
     assert_response(:success)
-    assert_equal(JSON.parse(response.body)['light'], 'off')
   end
 
 end
