0cf08560e16f64fa9318b3a3d11c8a76d94d9ce2
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-02 20:01:01 +0200

    refactor(routes): set #live method as the application's root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'screen/write'
-
-  get 'static_pages/camera'
-
   # Routes to turn light on and off
   get 'light/:light_id/on', to: 'light#on'
   get 'light/:light_id/off', to: 'light#off'
+
+  root 'static_pages#live'
+
+  post 'lcd/write'
 
 end
