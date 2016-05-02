929cb110afc0a9ccd0c503a299b7a679b8e10b7a
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-02 20:01:56 +0200

    feat(live): replace camera view by live one + add form with input for text to display

--- app/services/lcd_service.rb
+++ app/services/lcd_service.rb
@@ -1,2 +1,10 @@
-class LCDService
+class LcdService
+
+  def send_text_to_display(text)
+    while text.present?
+      text_portion = text.slice!(0..15)
+      Arduino::SERIAL.serial_puts(text_portion)
+    end
+  end
+
 end
