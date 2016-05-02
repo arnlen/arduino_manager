c9464637de9962d25b1ea430fad3ebc3c905beb9
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-02 20:15:39 +0200

    fix(service): set #send_text_to_display as class method

--- app/services/lcd_service.rb
+++ app/services/lcd_service.rb
@@ -1,6 +1,6 @@
 class LcdService
 
-  def send_text_to_display(text)
+  def self.send_text_to_display(text)
     while text.present?
       text_portion = text.slice!(0..15)
       Arduino::SERIAL.serial_puts(text_portion)
