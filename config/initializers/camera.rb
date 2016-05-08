2141bf94cacdc2bd841d6dcf922b1dece4e3af93
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-08 18:11:44 +0200

    config(URL): add new URL for camera live footage

--- config/initializers/camera.rb
+++ config/initializers/camera.rb
@@ -1,5 +1,5 @@
 # Camera live footage URL
 
 module Camera
-  URL = "http://projets.le-toubib-du-web.fr:8081"
+  URL = "http://projets.le-toubib-du-web.fr/01-live-cam-lcd/camera"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#live'
 
   # Routes to turn light on and off
   get 'light/:light_id/on', to: 'light#on'
   get 'light/:light_id/off', to: 'light#off'
-
-  root 'static_pages#live'
 
   post 'lcd/write'
 
